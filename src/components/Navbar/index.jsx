34f56273f74691f2f02fe860155384608c2f5a78
Thanh Pham <thanhtanpha@gmail.com>
2024-08-03 13:29:39 -0400

    fix path

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   const basePath = '/react-cgecko-api'
-  
+
   return (
     <>
       <Navbar bg="primary" data-bs-theme="dark">
@@ -12,7 +12,7 @@ const Header = () => {
           <Navbar.Brand href={`${basePath}/`}>React Boostrap</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href={`${basePath}/`}>Home</Nav.Link>
-            <Nav.Link href={`${basePath}/`}>Contact</Nav.Link>
+            <Nav.Link href={`${basePath}/contact`}>Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
